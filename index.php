54f1cdccd3ed71506ec9416fed65b25691c7e450
Alex Wallace <alexwallax@gmail.com>
2021-11-16 15:01:11 -0300

    primeiro commit inicializando o projeto de login

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+    //para da acesso a págian index.php apenas quem tiver acesso
+    session_start();
+
+    //$_SESSIOn variavel glogal que guarda os dados de sessão
+    if(isset($_SESSION['id']) && empty($_SESSION['id']) == false) {
+        echo "Área restrita..."; //so vai entrar se estiver logado
+    } else {
+        header("Location: login.php"); //redirecionar para página de login.php
+    }
+
+
+
+?>
